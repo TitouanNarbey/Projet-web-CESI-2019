ed0c0e258a84fc0a4bb94dbbeb75daa17fc9dee3
Loïs Cèbe <lois.cebe@viacesi.fr>
2019-11-08 14:40:47 +0100

    Add routes

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,44 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+//Route de Test
 Route::get('blade', function () {
     return view('child');
 });
+Route::get('login', function () {
+    return view('login');
+});
+Route::get('register', function () {
+    return view('register');
+});
+Route::get('profile', function () {
+    return view('profile');
+});
+Route::get('cart', function () {
+    return view('cart');
+});
+Route::get('legal', function () {
+    return view('legal');
+});
+//Benjamin, n'hésite pas à modifier les routes en fonction de ce que tu as besoin pour la partie légal
+Route::get('legal/cookies', function () {
+    return view('legalcookies');
+});
+Route::get('legal/CGV', function () {
+    return view('legalcgv');
+});
+Route::get('shop', function () {
+    return view('shop');
+});
+Route::get('shop/{id}', function () {
+    return view('article');
+});
+Route::get('events', function () {
+    return view('events');
+});
+Route::get('events/{id}', function () {
+    return view('event');
+});
+
