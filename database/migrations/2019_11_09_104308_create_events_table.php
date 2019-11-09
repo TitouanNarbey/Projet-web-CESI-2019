2cca19950bd1781163b286e5c67cc245219cd674
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-09 11:58:07 +0100

    Add events; orders; posts

---
+++ database/migrations/2019_11_09_104308_create_events_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('description', 2047);
+            $table->date('start_date');
+            $table->date('end_date')->nullable($value = true);
+            $table->float('price', 11, 2);
+            $table->boolean('recurrent');
+            $table->boolean('validate');
+
+            $table->unsignedBigInteger('id_images');
+            $table->foreign('id_images')->references('id')->on('images');
+
+            $table->unsignedBigInteger('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
---
+++ database/migrations/2019_11_09_105406_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('text', 2047);
+            $table->date('date');
+
+            $table->unsignedBigInteger('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+
+            $table->unsignedBigInteger('id_events');
+            $table->foreign('id_events')->references('id')->on('events');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
