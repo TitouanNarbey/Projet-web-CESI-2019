cc505cdd53414689c927cb7015fb7e5abd5e8246
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-09 11:15:53 +0100

    first create_table in right order (campus; roles; images; users) , go test in not the right order

---
+++ database/migrations/2019_11_09_095155_create_campus_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('campus', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 255);
+            $table->string('address', 255);
+            $table->string('city', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('campus');
+    }
+}
