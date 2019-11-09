e8f674572e135a1b86807a19bcbcdb164f8d15f8
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-09 14:59:22 +0100

    start le get de BDD avec clef étrangères (campus de user) | utiliser: ->campus->name

---
+++ app/Campus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Campus extends Model
+{
+    protected $table = 'campus';
+    //protected $primaryKey = 'id';
+    public $timestamps = false;
+
+}
--- app/User.php
+++ app/User.php
@@ -6,34 +6,42 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+use Illuminate\Database\Eloquent\Model;
+
 class User extends Authenticatable
 {
-    use Notifiable;
+    protected $table = 'users';
+    //protected $primaryKey = 'id';
+    //public $timestamps = false;
 
+
+    public function campus(){
+    return $this->belongsTo('App\Campus', 'id_campus', 'id');
+    }
+
+    //use Notifiable;
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
+    /*protected $fillable = [
         'name', 'email', 'password',
-    ];
-
+    ];*/
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    protected $hidden = [
+    /*protected $hidden = [
         'password', 'remember_token',
-    ];
-
+    ];*/
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-    protected $casts = [
+    /*protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
+    ];*/
 }
