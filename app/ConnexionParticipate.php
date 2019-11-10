654912baacc8ffd1d482c6ca65acf2fd6614bdca
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-10 09:04:38 +0100

    add des connexion pour les tables de simple connexion

---
+++ app/ConnexionParticipate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ConnexionParticipate extends Model
+{
+    protected $table = 'participate';
+    //protected $primaryKey = 'id';
+    //public $timestamps = false;
+    protected $fillable = ['id_events', 'id_users'];
+}
