0ab57e646210f4377159342764d6485ecf82a98d
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-10 10:07:39 +0100

    fill data for images

---
+++ database/migrations/2019_11_10_085754_fill_data_images.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FillDataImages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $obj = App\Image::create(['path'=>'assets/img/public/aafflla.png', 'alt'=>'yhwrx']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffllz.png', 'alt'=>'hwx']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aafflle.png', 'alt'=>'wnd ']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffllr.png', 'alt'=>'nwx,fj']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffllt.png', 'alt'=>'sj sbtd']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffa.png', 'alt'=>'sryj,n']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffz.png', 'alt'=>'tnduj   jt,n']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffe.png', 'alt'=>'yxeutcyvik']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aaffr.png', 'alt'=>'xjutcr']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjt.png', 'alt'=>'jcnfuk']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjy.png', 'alt'=>'x,jxyj']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajju.jpg', 'alt'=>'x,hykxy']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajji.jpg', 'alt'=>'kxy,y,j']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjo.jpg', 'alt'=>'Lorem']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjp.jpg', 'alt'=>'tbey']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjq.jpg', 'alt'=>'</body>']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjs.jpg', 'alt'=>'vwyx']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjd.jpg', 'alt'=>'jxcn']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjf.jpg', 'alt'=>'ky,c']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjlla.jpg', 'alt'=>'jwxhc']);
+        $obj = App\Image::create(['path'=>'assets/img/public/aajjllz.jpg', 'alt'=>'kxjg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjlle.jpg', 'alt'=>'jxh']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjllr.jpg', 'alt'=>'jwhxhx']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjja.jpg', 'alt'=>'hwxgbg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjz.png', 'alt'=>'byjnchn']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjje.png', 'alt'=>'jwg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffa.png', 'alt'=>',xjgfb']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffz.png', 'alt'=>'hnxhb']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffe.png', 'alt'=>'jcnhwd']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffr.png', 'alt'=>'gsrwev']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjfft.png', 'alt'=>'wsbg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffy.png', 'alt'=>'whrfbgwd']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjfflla.png', 'alt'=>'hwbxfv']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjffllz.png', 'alt'=>'hwbxv']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjfflle.jpg', 'alt'=>'<shbg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmmlla.jpg', 'alt'=>'whsg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmmllz.jpg', 'alt'=>'wgvgw']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmmlle.jpg', 'alt'=>'hqswgx']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmma.png', 'alt'=>'<gwg<gw>gG>gg']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmmz.png', 'alt'=>'<br><br><br>']);
+        $obj = App\Image::create(['path'=>'assets/img/public/bbjjmme.png', 'alt'=>'</html>']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
