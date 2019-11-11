ef8a5ff5b3bb85970e84807fc09f20f861994490
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-11 11:24:35 +0100

    fill data for voted and participated + go modif MCD (commented point on illustrateeventsmulti)

---
+++ database/migrations/2019_11_11_085107_fill_data_voted.php
@@ -0,0 +1,161 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FillDataVoted extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'22']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'23']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'19']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'115']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'118']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'134']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'153']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'3', 'id_users'=>'189']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'22']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'23']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'19']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'115']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'118']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'134']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'153']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'189']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'22']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'23']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'19']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'115']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'118']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'134']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'153']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'189']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'9', 'id_users'=>'22']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'9', 'id_users'=>'23']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'9', 'id_users'=>'134']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'9', 'id_users'=>'153']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'9', 'id_users'=>'19']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'22']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'23']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'189']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'19']);
+
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'1']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'2']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'3']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'4']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'14']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'6']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'7']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'8']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'1', 'id_users'=>'9']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'10']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'11']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'12']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'13']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'14']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'15']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'16']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'17']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'18']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'21']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'25']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'26']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'2', 'id_users'=>'27']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'120']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'121']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'122']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'123']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'124']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'125']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'126']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'127']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'128']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'129']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'130']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'4', 'id_users'=>'131']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'120']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'121']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'121']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'11']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'12']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'13']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'15']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'18']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'117']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'17']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'140']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'5', 'id_users'=>'141']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'142']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'143']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'144']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'145']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'146']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'147']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'148']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'149']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'6', 'id_users'=>'156']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'156']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'149']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'170']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'171']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'172']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'7', 'id_users'=>'173']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'174']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'175']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'176']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'177']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'178']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'179']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'170']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'8', 'id_users'=>'171']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'160']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'161']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'162']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'163']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'164']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'165']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'10', 'id_users'=>'166']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'160']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'161']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'162']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'163']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'164']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'165']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'166']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'11', 'id_users'=>'167']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'160']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'161']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'162']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'163']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'164']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'165']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'166']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'167']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'168']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'12', 'id_users'=>'169']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'160']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'161']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'162']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'163']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'164']);
+        $obj = App\ConnexionVoted::create(['id_events'=>'14', 'id_users'=>'165']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
