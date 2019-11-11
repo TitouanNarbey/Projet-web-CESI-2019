c083de84811474686cc7d4f025558d660a8e81b0
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-11 09:41:21 +0100

    fill data for illustratearticlesmulti

---
+++ database/migrations/2019_11_11_083114_fill_data_illustratearticlesmulti.php
@@ -0,0 +1,111 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FillDataIllustratearticlesmulti extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'1', 'id_articles'=>'225']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'1', 'id_articles'=>'226']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'1', 'id_articles'=>'227']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'1', 'id_articles'=>'228']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'2', 'id_articles'=>'229']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'2', 'id_articles'=>'230']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'2', 'id_articles'=>'231']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'3', 'id_articles'=>'232']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'3', 'id_articles'=>'233']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'3', 'id_articles'=>'234']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'4', 'id_articles'=>'235']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'4', 'id_articles'=>'236']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'4', 'id_articles'=>'237']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'4', 'id_articles'=>'238']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'5', 'id_articles'=>'239']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'5', 'id_articles'=>'240']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'5', 'id_articles'=>'241']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'6', 'id_articles'=>'242']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'6', 'id_articles'=>'243']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'6', 'id_articles'=>'244']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'6', 'id_articles'=>'245']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'7', 'id_articles'=>'246']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'7', 'id_articles'=>'247']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'7', 'id_articles'=>'248']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'8', 'id_articles'=>'249']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'8', 'id_articles'=>'250']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'8', 'id_articles'=>'301']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'9', 'id_articles'=>'302']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'9', 'id_articles'=>'303']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'9', 'id_articles'=>'304']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'9', 'id_articles'=>'305']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'10', 'id_articles'=>'306']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'10', 'id_articles'=>'307']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'10', 'id_articles'=>'308']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'10', 'id_articles'=>'309']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'11', 'id_articles'=>'310']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'11', 'id_articles'=>'311']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'11', 'id_articles'=>'312']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'12', 'id_articles'=>'313']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'12', 'id_articles'=>'314']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'12', 'id_articles'=>'315']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'13', 'id_articles'=>'316']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'13', 'id_articles'=>'317']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'13', 'id_articles'=>'318']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'14', 'id_articles'=>'319']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'14', 'id_articles'=>'320']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'14', 'id_articles'=>'321']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'15', 'id_articles'=>'322']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'15', 'id_articles'=>'323']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'15', 'id_articles'=>'324']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'16', 'id_articles'=>'325']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'16', 'id_articles'=>'326']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'16', 'id_articles'=>'327']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'17', 'id_articles'=>'328']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'17', 'id_articles'=>'329']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'17', 'id_articles'=>'330']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'18', 'id_articles'=>'331']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'18', 'id_articles'=>'332']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'18', 'id_articles'=>'333']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'19', 'id_articles'=>'334']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'19', 'id_articles'=>'335']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'19', 'id_articles'=>'336']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'19', 'id_articles'=>'337']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'20', 'id_articles'=>'338']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'20', 'id_articles'=>'339']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'20', 'id_articles'=>'340']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'20', 'id_articles'=>'341']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'21', 'id_articles'=>'342']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'21', 'id_articles'=>'343']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'21', 'id_articles'=>'344']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'22', 'id_articles'=>'345']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'22', 'id_articles'=>'346']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'22', 'id_articles'=>'347']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'22', 'id_articles'=>'348']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'23', 'id_articles'=>'349']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'23', 'id_articles'=>'350']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'23', 'id_articles'=>'351']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'24', 'id_articles'=>'352']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'24', 'id_articles'=>'353']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'24', 'id_articles'=>'354']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'25', 'id_articles'=>'355']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'25', 'id_articles'=>'356']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'25', 'id_articles'=>'357']);
+        $obj = App\ConnexionIllustratearticlesmulti::create(['id_images'=>'25', 'id_articles'=>'358']);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
