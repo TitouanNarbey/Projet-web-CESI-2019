9633a586fcd07163e26ba1f94acdd14efdd81149
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-11 14:18:14 +0100

    fill data comanded

--- database/migrations/2019_11_11_085037_fill_data_comanded.php
+++ database/migrations/2019_11_11_085037_fill_data_comanded.php
@@ -13,7 +13,80 @@ class FillDataComanded extends Migration
      */
     public function up()
     {
-        //
+        $obj = App\Comanded::create(['id_orders'=>'1', 'id_articles'=>'1', 'quantity'=>'1']);
+        $obj = App\Comanded::create(['id_orders'=>'2', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'3', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'4', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'5', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'6', 'id_articles'=>'1', 'quantity'=>'3']);
+        $obj = App\Comanded::create(['id_orders'=>'7', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'8', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'9', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'10', 'id_articles'=>'1', 'quantity'=>'8']);
+        $obj = App\Comanded::create(['id_orders'=>'11', 'id_articles'=>'1', 'quantity'=>'1']);
+        $obj = App\Comanded::create(['id_orders'=>'12', 'id_articles'=>'1', 'quantity'=>'1']);
+        $obj = App\Comanded::create(['id_orders'=>'13', 'id_articles'=>'1', 'quantity'=>'3']);
+        $obj = App\Comanded::create(['id_orders'=>'14', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'15', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'16', 'id_articles'=>'1', 'quantity'=>'8']);
+        $obj = App\Comanded::create(['id_orders'=>'17', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'18', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'19', 'id_articles'=>'1', 'quantity'=>'3']);
+        $obj = App\Comanded::create(['id_orders'=>'20', 'id_articles'=>'1', 'quantity'=>'11']);
+        $obj = App\Comanded::create(['id_orders'=>'21', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'22', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'23', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'24', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'25', 'id_articles'=>'1', 'quantity'=>'15']);
+        $obj = App\Comanded::create(['id_orders'=>'26', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'27', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'28', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'29', 'id_articles'=>'1', 'quantity'=>'1']);
+        $obj = App\Comanded::create(['id_orders'=>'30', 'id_articles'=>'1', 'quantity'=>'3']);
+        $obj = App\Comanded::create(['id_orders'=>'31', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'32', 'id_articles'=>'1', 'quantity'=>'8']);
+        $obj = App\Comanded::create(['id_orders'=>'33', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'34', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'35', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'36', 'id_articles'=>'1', 'quantity'=>'8']);
+        $obj = App\Comanded::create(['id_orders'=>'37', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'38', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'39', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'43', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'44', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'45', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'46', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'47', 'id_articles'=>'1', 'quantity'=>'7']);
+        $obj = App\Comanded::create(['id_orders'=>'48', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'49', 'id_articles'=>'1', 'quantity'=>'14']);
+        $obj = App\Comanded::create(['id_orders'=>'50', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'51', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'52', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'53', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'54', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'55', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'56', 'id_articles'=>'1', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'57', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'59', 'id_articles'=>'1', 'quantity'=>'8']);
+        $obj = App\Comanded::create(['id_orders'=>'62', 'id_articles'=>'1', 'quantity'=>'9']);
+        $obj = App\Comanded::create(['id_orders'=>'63', 'id_articles'=>'1', 'quantity'=>'3']);
+        $obj = App\Comanded::create(['id_orders'=>'64', 'id_articles'=>'1', 'quantity'=>'1']);
+        $obj = App\Comanded::create(['id_orders'=>'65', 'id_articles'=>'1', 'quantity'=>'4']);
+        $obj = App\Comanded::create(['id_orders'=>'66', 'id_articles'=>'1', 'quantity'=>'5']);
+        $obj = App\Comanded::create(['id_orders'=>'67', 'id_articles'=>'1', 'quantity'=>'6']);
+        $obj = App\Comanded::create(['id_orders'=>'68', 'id_articles'=>'1', 'quantity'=>'9']);
+
+        $obj = App\Comanded::create(['id_orders'=>'58', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'8', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'60', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'61', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'67', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'40', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'41', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'42', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'56', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'5', 'id_articles'=>'16', 'quantity'=>'2']);
+        $obj = App\Comanded::create(['id_orders'=>'65', 'id_articles'=>'16', 'quantity'=>'2']);
     }
 
     /**
