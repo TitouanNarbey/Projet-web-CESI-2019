0870369607927489a8afba9786f8f4b68899379b
Ben <ben.mollon@gmail.com>
2019-11-12 10:43:33 +0100

    12/11/2019 10:43 Ben

--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -63,8 +63,7 @@
 	</div>
 	<div class="row">
 		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-			<span>Cèbe Loïs<b></b></span>
-			met en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de , et signaler toutes modifications du site qu'il jugerait utile. n'est en aucun cas responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
+			Cèbe Loïsmet en œuvre tous les moyens dont elle dispose, pour assurer une information fiable et une mise à jour fiable de ses sites internet. Toutefois, des erreurs ou omissions peuvent survenir. L'internaute devra donc s'assurer de l'exactitude des informations auprès de , et signaler toutes modifications du site qu'il jugerait utile. n'est en aucun cas responsable de l'utilisation faite de ces informations, et de tout préjudice direct ou indirect pouvant en découler.
 		</div>
 	</div>
 	<div class="row">
@@ -96,73 +95,69 @@
 	</div>
 	<div class="row">
 		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				L'ensemble des activités ainsi que ses informations sont présentés sur notre site 
-				<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
-				Cèbe Loïs s’efforce de fournir sur le site www.bdecesilyon.fr des informations aussi précises que possible. les renseignements figurant sur le site 
-				<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
-				 ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, tous les informations indiquées sur le site www.bdecesilyon.fr
-				sont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis.  
-			</div>
+			L'ensemble des activités ainsi que ses informations sont présentés sur notre site 
+			<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
+			Cèbe Loïs s’efforce de fournir sur le site www.bdecesilyon.fr des informations aussi précises que possible. les renseignements figurant sur le site 
+			<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
+			 ne sont pas exhaustifs et les photos non contractuelles. Ils sont donnés sous réserve de modifications ayant été apportées depuis leur mise en ligne. Par ailleurs, tous les informations indiquées sur le site www.bdecesilyon.frsont données à titre indicatif, et sont susceptibles de changer ou d’évoluer sans préavis.  
 		</div>
-		<div class="row">
-			<div class="title">
-				Limitation contractuelles sur les données :
-			</div>
+	</div>
+	<div class="row">
+		<div class="title">
+			Limitation contractuelles sur les données :
 		</div>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse bdecesilyon@gmail.com, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement. 
-				<span>De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour</span>
-				Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de Cèbe Loïs.
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
+			Les informations contenues sur ce site sont aussi précises que possible et le site remis à jour à différentes périodes de l’année, mais peut toutefois contenir des inexactitudes ou des omissions. Si vous constatez une lacune, erreur ou ce qui parait être un dysfonctionnement, merci de bien vouloir le signaler par email, à l’adresse bdecesilyon@gmail.com, en décrivant le problème de la manière la plus précise possible (page posant problème, type d’ordinateur et de navigateur utilisé, …).Tout contenu téléchargé se fait aux risques et périls de l'utilisateur et sous sa seule responsabilité. En conséquence, ne saurait être tenu responsable d'un quelconque dommage subi par l'ordinateur de l'utilisateur ou d'une quelconque perte de données consécutives au téléchargement. De plus, l’utilisateur du site s’engage à accéder au site en utilisant un matériel récent, ne contenant pas de virus et avec un navigateur de dernière génération mis-à-jour.Les liens hypertextes mis en place dans le cadre du présent site internet en direction d'autres ressources présentes sur le réseau Internet ne sauraient engager la responsabilité de Cèbe Loïs.
 		</div>
-		<div class="row">
-			<div class="title">
-				Propriété intellectuelle :
-			</div>
+	</div>
+	<div class="row">
+		<div class="title">
+			Propriété intellectuelle :
 		</div>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				Tout le contenu du présent sur le site 
-				<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
-				, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la Cèbe Loïs à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Cèbe Loïs. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
+			Tout le contenu du présent sur le site 
+			<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
+			, incluant, de façon non limitative, les graphismes, images, textes, vidéos, animations, sons, logos, gifs et icônes ainsi que leur mise en forme sont la propriété exclusive de la Cèbe Loïs à l'exception des marques, logos ou contenus appartenant à d'autres sociétés partenaires ou auteurs.Toute reproduction, distribution, modification, adaptation, retransmission ou publication, même partielle, de ces différents éléments est strictement interdite sans l'accord exprès par écrit de Cèbe Loïs. Cette représentation ou reproduction, par quelque procédé que ce soit, constitue une contrefaçon sanctionnée par les articles L.335-2 et suivants du Code de la propriété intellectuelle. Le non-respect de cette interdiction constitue une contrefaçon pouvant engager la responsabilité civile et pénale du contrefacteur. En outre, les propriétaires des Contenus copiés pourraient intenter une action en justice à votre encontre.
 		</div>
-		<div class="row">
-			<div class="title">
-				Déclaration à la CNIL :
-			</div>
+	</div>
+	<div class="row">
+		<div class="title">
+			Déclaration à la CNIL :
 		</div>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, ce site n'a pas fait l'objet d'une déclaration  auprès de la Commission nationale de l'informatique et des libertés (
-				<a href="http://www.cnil.fr/">www.cnil.fr</a>
-				).
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
+			Conformément à la loi 78-17 du 6 janvier 1978 (modifiée par la loi 2004-801 du 6 août 2004 relative à la protection des personnes physiques à l'égard des traitements de données à caractère personnel) relative à l'informatique, aux fichiers et aux libertés, ce site n'a pas fait l'objet d'une déclaration  auprès de la Commission nationale de l'informatique et des libertés (
+			<a href="http://www.cnil.fr/">www.cnil.fr</a>
+			).
 		</div>
-		<div class="row">
-			<div class="title">
-				Litiges :
-			</div>
+	</div>
+	<div class="row">
+		<div class="title">
+			Litiges :
 		</div>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				Les présentes conditions du site 
-				<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
-				 sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux. La langue de référence, pour le règlement de contentieux éventuels, est le français.
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
+			Les présentes conditions du site 
+			<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
+			 sont régies par les lois françaises et toute contestation ou litiges qui pourraient naître de l'interprétation ou de l'exécution de celles-ci seront de la compétence exclusive des tribunaux. La langue de référence, pour le règlement de contentieux éventuels, est le français.
 		</div>
-		<div class="row">
-			<div class="title">
-				Données personnelles :
-			</div>
+	</div>
+	<div class="row">
+		<div class="title">
+			Données personnelles :
 		</div>
-		<div class="row">
-			<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
-				De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet 
-				<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
-				.Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « contact ». Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, notamment celui de solliciter des renseignements sur nos activités, ou de recevoir les lettres d’information.Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d'accès. De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
-			</div>
+	</div>
+	<div class="row">
+		<div class="col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
+			De manière générale, vous n’êtes pas tenu de nous communiquer vos données personnelles lorsque vous visitez notre site Internet 
+			<a href="http://www.bdecesilyon.fr">www.bdecesilyon.fr</a>
+			.Cependant, ce principe comporte certaines exceptions. En effet, pour certains services proposés par notre site, vous pouvez être amenés à nous communiquer certaines données telles que : votre nom, votre fonction, le nom de votre société, votre adresse électronique, et votre numéro de téléphone. Tel est le cas lorsque vous remplissez le formulaire qui vous est proposé en ligne, dans la rubrique « contact ». Dans tous les cas, vous pouvez refuser de fournir vos données personnelles. Dans ce cas, vous ne pourrez pas utiliser les services du site, notamment celui de solliciter des renseignements sur nos activités, ou de recevoir les lettres d’information.Enfin, nous pouvons collecter de manière automatique certaines informations vous concernant lors d’une simple navigation sur notre site Internet, notamment : des informations concernant l’utilisation de notre site, comme les zones que vous visitez et les services auxquels vous accédez, votre adresse IP, le type de votre navigateur, vos temps d'accès. De telles informations sont utilisées exclusivement à des fins de statistiques internes, de manière à améliorer la qualité des services qui vous sont proposés. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
 		</div>
 	</div>
 </div>
