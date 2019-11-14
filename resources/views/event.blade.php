7f526b4bfcd0eb24c382d60e60e3cca0a8057fcb
Loïs Cèbe <lois.cebe@viacesi.fr>
2019-11-14 21:29:48 +0100

    Button départiciper

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -66,11 +66,20 @@ Ajouter la fonction télécharger une image et cacher le bouton en fonction du r
 <h5>
     Envoie nous les !!!!
 </h5>
+
+ 
 <button type="button" class="btn btn-cesi  mt-1 mb-3 "><i class="fas fa-upload"></i> Ajouter une image</button>
-     <form class="example" action="/events/{{$event->id}}" method="post">
-       @csrf
-       <input class ="btn btn-success mt-1 mb-1" type="submit" value="Participer">
-     </form>
+@if(session()->has('participate'))
+  <form class="example" action="/events/{{$event->id}}" method="post">
+    @csrf
+    <input class ="btn btn-danger mt-1 mb-1" type="submit" value="Ne plus participer">
+  </form>
+@else
+  <form class="example" action="/events/{{$event->id}}" method="post">
+    @csrf
+    <input class ="btn btn-success mt-1 mb-1" type="submit" value="Participer">
+  </form>
+@endif
 </div>
 </div>
 <!-- Espace Commentaire -->
