989e80e3c324f3a98ee77eb284e11d8956b71145
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-15 21:48:03 +0100

    downloadAllImages restraint au role == 3

--- app/Http/Controllers/DownloadController.php
+++ app/Http/Controllers/DownloadController.php
@@ -5,45 +5,60 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Image;
 use ZipArchive;
+use Auth;
 
 class DownloadController extends Controller
 {
 	public function downloadAllImages(){
 
-		$files = array(); /*Image array*/
+		if(Auth::user() !== null)
+        {
+            if(Auth::user()->id_roles == 3)
+            {
+				$files = array(); /*Image array*/
 
-		$images = Image::all();
+				$images = Image::all();
 
-		foreach($images as $image)
-		{
-			if($image->path[0] == '/')
+				foreach($images as $image)
+				{
+					if($image->path[0] == '/')
+					{
+						array_push($files, '.'.$image->path);
+					}
+				}
+			
+				# create new zip opbject
+				$zip = new ZipArchive();
+			
+				# create a temp file & open it
+				$tmp_file = tempnam('/tmp/','');
+				$zip->open($tmp_file, ZipArchive::CREATE);
+
+				# loop through each file
+				foreach($files as $file)
+				{
+					# download file
+					$download_file = file_get_contents($file);
+					#add it to the zip
+					$zip->addFromString(basename($file),$download_file);
+				}
+
+				# close zip
+				$zip->close();
+
+				# send the file to the browser as a download
+				header('Content-disposition: attachment; filename=BDE-CESI-Lyon_AllImages.zip');
+				header('Content-type: application/zip');
+				readfile($tmp_file);
+			}
+			else
 			{
-				array_push($files, '.'.$image->path);
+				return redirect('/home')->with('messageRed', 'Vous n\'avez pas le droit d\'accéder à cette page.');
 			}
 		}
-	
-		# create new zip opbject
-		$zip = new ZipArchive();
-	
-		# create a temp file & open it
-		$tmp_file = tempnam('/tmp/','');
-		$zip->open($tmp_file, ZipArchive::CREATE);
-
-		# loop through each file
-		foreach($files as $file)
+		else
 		{
-			# download file
-			$download_file = file_get_contents($file);
-			#add it to the zip
-			$zip->addFromString(basename($file),$download_file);
+			return redirect('/home')->with('messageRed', 'Vous n\'avez pas le droit d\'accéder à cette page.');
 		}
-
-		# close zip
-		$zip->close();
-
-		# send the file to the browser as a download
-		header('Content-disposition: attachment; filename=BDE-CESI-Lyon_AllImages.zip');
-		header('Content-type: application/zip');
-		readfile($tmp_file);
 	}
 }
