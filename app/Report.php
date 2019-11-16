4519939551ee0a76e63243bfdfb7f1bae476f718
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-16 19:40:35 +0100

    complete Report model

--- app/Report.php
+++ app/Report.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Report extends Model
 {
-    //
+    protected $table = 'report';
+    //protected $primaryKey = 'id';
+    //public $timestamps = false;
+    protected $fillable = ['id_users', 'text'];
 }
