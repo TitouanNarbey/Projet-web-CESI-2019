74c1c898bf02d4c5dc8443cb5884389a0b092bd3
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-16 19:47:09 +0100

    create table for report

--- app/Report.php
+++ app/Report.php
@@ -9,5 +9,9 @@ class Report extends Model
     protected $table = 'report';
     //protected $primaryKey = 'id';
     //public $timestamps = false;
-    protected $fillable = ['id_users', 'text'];
+    protected $fillable = ['id_users', 'text', 'created_at', 'updated_at'];
+
+    public function users(){
+        return $this->BelongTo('App\User', 'id_users', 'id');
+    }
 }
---
+++ database/migrations/2019_11_16_184145_create_report_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('text', 4095);
+            
+            $table->unsignedBigInteger('id_users');
+            $table->foreign('id_users')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+}
