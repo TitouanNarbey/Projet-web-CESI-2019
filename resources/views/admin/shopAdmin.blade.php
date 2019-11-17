f1e1293a455f6f6cf0ea0fe56aea05b2643ddaa0
TitouanNarbey <titouan.narbey@gmail.com>
2019-11-17 10:02:47 +0100

    remise en page

--- resources/views/admin/shopAdmin.blade.php
+++ resources/views/admin/shopAdmin.blade.php
@@ -1,63 +1,47 @@
 <!-- Faire fonctionner tous les boutons présents -->
-
-
 @extends('layouts.app')@section('content')
 <div class="container col-11">
-  <!-- Fonctions de modération -->
-  <div class="description text-center">
-    <h1>Administration de la boutique</h1>
-    <button class="btn btn-success "> Ajouter un article</button>
-    <div class=" dropdown mt-2">
-      <button onclick="showDropdownPrice()" class=" btn btn-cesi">Trier par prix</button>
-      <div id="price" class="dropdown-content">
-        <a href="#about">
-          <button class="btn btn-cesi btn-block">Croissant</button>
-          <a href="#about"><button class="btn btn-cesi btn-block">Décroissant</button></div>
-        </div>
-        <div class=" dropdown ">
-          <button onclick="showDropdownCategory()" class=" btn btn-cesi">Catégorie</button>
-          <div id="category" class="dropdown-content">
-            @foreach($shop as $article)
-            <a href="#about"><button class="btn btn-cesi btn-block"> {{$article->category->name}}</button>  </a>
-          @endforeach</div>
-          </div>
-        </div>
-        <!-- Affichage de la liste des articles -->
-        <div class="row mx-auto">
-            @foreach($shop as $article)
-          <div class="col-xl-3 mt-5">
-            <div class="card {{$article->category}}">
-                <div class="headerCardEvent">
-                  <img src="{{$article->image->path}}" class="img-fluid sizeBanner" alt="{{$article->image->alt}}">
-                </div>
-                <div class="card-body">
-                  <h3>
-                    {{$article->
-                    name}}
-                  </h3>
-                  <div class="eventCardDescription">
-                    {{$article->
-                    description}}
-                  </div>
-                </div>
-                <div class="card-footer">
-                  <div class="text-right eventCardFooter">
-                    <a href="#about"><button class="btn btn-danger btn-block"> Supprimer l'article</button></a></div>
-                  </div>
-                
-              </div>
-            </div>
-            @endforeach
-          </div>
-          <!-- Pagination -->
-          <nav aria-label="Page navigation example">
-            <ul class="pagination justify-content-center mt-5">
-              <li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>
-              <li class="page-item"><a class="page-link" href="#">1</a></li>
-              <li class="page-item"><a class="page-link" href="#">2</a></li>
-              <li class="page-item"><a class="page-link" href="#">3</a></li>
-              <li class="page-item"><a class="page-link" href="#">Next</a></li>
-            </ul>
-          </nav>
-        </div>
+	<!-- Fonctions de modération -->
+	<div class="description text-center">
+		<h1>Administration de la boutique</h1>
+		<button class="btn btn-success "> Ajouter un article</button>
+		<div class=" dropdown mt-2">
+			<button onclick="showDropdownPrice()" class=" btn btn-cesi">Trier par prix</button>
+			<div id="price" class="dropdown-content">
+				<a href="#about">
+					<button class="btn btn-cesi btn-block">Croissant</button>
+				<a href="#about"><button class="btn btn-cesi btn-block">Décroissant</button>
+			</div>
+		</div>
+		<div class=" dropdown ">
+			<button onclick="showDropdownCategory()" class=" btn btn-cesi">Catégorie</button>
+			<div id="category" class="dropdown-content">
+				@foreach($shop as $article)
+					<a href="#about"><button class="btn btn-cesi btn-block"> {{$article->category->name}}</button></a>
+				@endforeach
+			</div>
+		</div>
+	</div>
+	<!-- Affichage de la liste des articles -->
+	<div class="row mx-auto">
+		@foreach($shop as $article)
+		<div class="col-xl-3 mt-5">
+			<div class="card {{$article->category}}">
+				<div class="headerCardEvent">
+					<img src="{{$article->image->path}}" class="img-fluid sizeBanner" alt="{{$article->image->alt}}">
+				</div>
+				<div class="card-body">
+					<h3>{{$article->name}}</h3>
+					<div class="eventCardDescription">{{$article->description}}</div>
+				</div>
+				<div class="card-footer">
+					<div class="text-right eventCardFooter">
+						<a href="#about"><button class="btn btn-danger btn-block"> Supprimer l'article</button></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		@endforeach
+	</div>
+</div>
 @endsection
